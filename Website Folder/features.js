9edac5d42271ac5450e047e89559115bdf77fdf8
Abhay <114037744+SenatorDingo@users.noreply.github.com>
2022-11-26 12:19:34 -0500

    Create features.js

---
+++ Website Folder/features.js
@@ -0,0 +1,48 @@
+function r1() {
+  var x = document.getElementById("B1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function r2() {
+  var x = document.getElementById("B2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function r3() {
+  var x = document.getElementById("B3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function r4() {
+  var x = document.getElementById("B4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function r5() {
+  var x = document.getElementById("B5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function r6() {
+  var x = document.getElementById("B6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
